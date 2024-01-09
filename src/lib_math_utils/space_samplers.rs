3a1b64915d0d619f84ed81a95365ac8dee25a8f3
William Gurecky <gureckywl@ornl.gov>
2024-01-08 20:17:33 -0500

    wip: unit tests for differential evolution mcmc

--- src/lib_math_utils/space_samplers.rs
+++ src/lib_math_utils/space_samplers.rs
@@ -5,8 +5,8 @@ use ndarray::prelude::*;
 use ndarray::{concatenate};
 use rand::prelude::*;
 use statrs::function::gamma;
-use rand::distributions::WeightedIndex;
-use rand_distr::Dirichlet;
+use rand::distributions::{WeightedIndex, Uniform};
+use rand_distr::{Dirichlet, Normal};
 use rayon::prelude::*;
 use assert_approx_eq::assert_approx_eq;
 
@@ -104,20 +104,22 @@ pub fn constr_dirichlet_sample(
 }
 
 struct McmcChain {
-    samples: Array2<f64>
+    samples: Array2<f64>,
+    id: usize,
 }
 
 impl McmcChain {
-    pub fn new(ndim: usize, init_s: ArrayView1<f64>) -> Self {
+    pub fn new(ndim: usize, init_s: ArrayView1<f64>, id: usize) -> Self {
         let mut init_samples: Array2<f64> = Array2::zeros((0, ndim));
         init_samples.push(Axis(0), init_s).unwrap();
         Self {
             samples: init_samples,
+            id: id,
         }
     }
 
     pub fn head_sample(&self) -> ArrayView1<f64> {
-        self.samples.row(self.samples.nrows())
+        self.samples.row(self.samples.nrows()-1)
     }
 
     pub fn append_sample(&mut self, sample: ArrayView1<f64>) {
@@ -125,20 +127,29 @@ impl McmcChain {
     }
 }
 
-struct DeMcSampler {
-    chains: Vec<McmcChain>,
-    chains_id: Vec<usize>,
-    gamma: f64,
-    n_chains: usize,
-    ndim: usize,
-    prop_fixup_fn: Option< fn(ArrayView1<f64>) -> Array1<f64> >,
-    ln_prob_fn: Box<dyn LnProbFn>,
-}
 
+/// Interface for log probability methods used in MCMC samplers
 trait LnProbFn {
-    fn lnp(&self, sample: ArrayView1<f64>, extra_args: Vec<f64>) -> f64;
+    fn lnp(&self, sample: ArrayView1<f64>, extra_args: &Vec<f64>) -> f64;
 }
 
+/// A custom likelihood function
+struct LnLikeCustom {
+    ln_like_fn: Box<dyn Fn(ArrayView1<f64>, &Vec<f64>)->f64>,
+}
+impl LnLikeCustom {
+    pub fn new(lnf: impl Fn(ArrayView1<f64>, &Vec<f64>)->f64 + 'static) -> Self {
+        Self { ln_like_fn: Box::new(lnf) }
+    }
+}
+impl LnProbFn for LnLikeCustom {
+    fn lnp(&self, sample: ArrayView1<f64>, extra_args: &Vec<f64>) -> f64
+    {
+        (self.ln_like_fn)(sample, extra_args)
+    }
+}
+
+/// A flat prior
 struct LnPriorUniform {
     bounds: Array2<f64>,
 }
@@ -150,7 +161,7 @@ impl LnPriorUniform {
     }
 }
 impl LnProbFn for LnPriorUniform {
-    fn lnp(&self, sample: ArrayView1<f64>, extra_args: Vec<f64>) -> f64
+    fn lnp(&self, sample: ArrayView1<f64>, extra_args: &Vec<f64>) -> f64
     {
         let mut valid = false;
         for (i, bound) in self.bounds.axis_iter(Axis(0)).enumerate() {
@@ -165,6 +176,7 @@ impl LnProbFn for LnPriorUniform {
     }
 }
 
+/// A Dirichlet MV likelihood
 struct LnLikeDirichlet {
     alpha: Array1<f64>,
 }
@@ -176,7 +188,7 @@ impl LnLikeDirichlet {
     }
 }
 impl LnProbFn for LnLikeDirichlet {
-    fn lnp(&self, sample: ArrayView1<f64>, extra_args: Vec<f64>) -> f64
+    fn lnp(&self, sample: ArrayView1<f64>, extra_args: &Vec<f64>) -> f64
     {
         // Diriclet PDF is only valid if sum(x)=1
         assert_approx_eq!(sample.sum(), 1.0);
@@ -194,8 +206,42 @@ impl LnProbFn for LnLikeDirichlet {
     }
 }
 
+/// Compose the numerator of the Posterior PDF (prior * likelihood)
+struct LnLikeSum {
+    like_fn: Box<dyn LnProbFn>,
+    prior_fn: Box<dyn LnProbFn>,
+}
+impl LnLikeSum {
+    pub fn new(like: impl LnProbFn + 'static, prior: impl LnProbFn + 'static) -> Self
+    {
+        Self {
+            like_fn: Box::new(like),
+            prior_fn: Box::new(prior),
+        }
+    }
+}
+impl LnProbFn for LnLikeSum {
+    fn lnp(&self, sample: ArrayView1<f64>, extra_args: &Vec<f64>) -> f64
+    {
+        self.like_fn.lnp(sample, extra_args) + self.prior_fn.lnp(sample, extra_args)
+    }
+}
+
+struct DeMcSampler {
+    chains: Vec<McmcChain>,
+    chains_id: Vec<usize>,
+    gamma: f64,
+    n_chains: usize,
+    ndim: usize,
+    var_epsilon: f64,
+    prop_fixup_fn: Option< Box<dyn Fn(ArrayView1<f64>)->Array1<f64>> >,
+    ln_prob_fn: Box<dyn LnProbFn>,
+    n_accept: usize,
+    n_reject: usize,
+}
+
 impl DeMcSampler {
-    pub fn new(ln_prob: impl LnProbFn + 'static, chains: Vec<McmcChain>, ndim: usize, gamma: f64)
+    pub fn new(ln_prob: impl LnProbFn + 'static, chains: Vec<McmcChain>, ndim: usize, gamma: f64, var_eps: f64)
         -> Self
     {
         let n_chains = chains.len();
@@ -210,17 +256,50 @@ impl DeMcSampler {
             gamma: gamma,
             n_chains: n_chains,
             ndim: ndim,
+            var_epsilon: var_eps,
             prop_fixup_fn: None,
             ln_prob_fn: Box::new(ln_prob),
+            n_accept: 0,
+            n_reject: 0,
         }
     }
 
-    pub fn set_prop_fixup(&mut self, fixup_fn: fn(ArrayView1<f64>) -> Array1<f64>) {
-        self.prop_fixup_fn = Some(fixup_fn);
+    pub fn set_prop_fixup(&mut self, fixup_fn: impl Fn(ArrayView1<f64>)->Array1<f64> + 'static) {
+        self.prop_fixup_fn = Some(Box::new(fixup_fn));
+    }
+
+    pub fn n_samples(&self) -> usize {
+        self.chains[0].samples.len()
+    }
+
+    /// Get the last n_tail samples from the given chain id
+    /// If n_tail = 0, returns all samples
+    pub fn get_chain_samples(&self, n_tail: usize, id: usize) -> Array2<f64> {
+        // self.chains[id].samples.ro
+        // self.chains[id].samples.view()
+        self.chains[id].samples.slice(s![self.n_samples()-n_tail-1.., ..]).to_owned()
+    }
+
+    /// Get the last n_tail samples from all chains
+    /// If n_tail = 0, returns all samples
+    pub fn get_samples(&self, n_tail: usize) -> Array2<f64> {
+        let mut tmp_out: Vec<Array2<f64>> = Vec::new();
+        for c in 0..self.n_chains {
+            tmp_out.push(self.get_chain_samples(n_tail, c));
+        }
+        let mut out: Array2<f64> = Array2::zeros((n_tail*self.n_chains, self.ndim));
+        let mut i: usize = 0;
+        for s in 0..n_tail {
+            for c in 0..self.n_chains {
+                out.row_mut(i).assign(&tmp_out[c].row(s));
+                i += 1;
+            }
+        }
+        out
     }
 
     /// draws one sample on specified chain
-    fn step_chain(&self, c_idx: usize) -> Array1<f64> {
+    fn step_chain(&self, c_idx: usize) -> (bool, Array1<f64>) {
         let c_sample = self.chains[c_idx].head_sample();
         let mut rng = rand::thread_rng();
         // select two random chains from chain pool that are not the current chain
@@ -232,11 +311,14 @@ impl DeMcSampler {
         let b_sample = self.chains[*sel_ids[1]].head_sample();
         let delta = a_sample.to_owned() - b_sample.to_owned();
         // inject small random noise
-        let var_ball: Array1<f64> = Array1::zeros(self.ndim);
+        let u_dist = Uniform::new(0.0, self.var_epsilon);
+        let uni_samples = (0..self.ndim).into_iter().map(|_i| { u_dist.sample(&mut rng) } ).collect();
+        let var_ball = Array1::from_vec(uni_samples);
+
         // construct proposal
         let mut prop_sample = c_sample.to_owned() + self.gamma * delta + var_ball;
         // optionally fix-up sample
-        match self.prop_fixup_fn {
+        match &self.prop_fixup_fn {
             Some(_fn) => { prop_sample = _fn(prop_sample.view()); }
             None => { }
         }
@@ -244,10 +326,10 @@ impl DeMcSampler {
         let alpha = self.accept_prob_ratio(c_sample.view(), prop_sample.view());
         let accept = self.metropolis_accept(alpha);
         if accept {
-            prop_sample
+            (true, prop_sample)
         }
         else {
-            c_sample.to_owned()
+            (false, c_sample.to_owned())
         }
     }
 
@@ -255,17 +337,28 @@ impl DeMcSampler {
     pub fn sample_mcmc(&mut self, n_samples: usize) {
         for _s in 0..n_samples {
             for c_i in 0..self.chains_id.len() {
-                let c_sample = self.step_chain(self.chains_id[c_i]);
+                let (act, c_sample) = self.step_chain(self.chains_id[c_i]);
+                if act {
+                    self.n_accept += 1;
+                }
+                else {
+                    self.n_reject += 1;
+                }
                 self.chains[c_i].append_sample(c_sample.view());
             }
         }
     }
 
+    /// Global acceptance ratio
+    pub fn accept_ratio(&self) -> f64 {
+        self.n_accept as f64 / (self.n_reject as f64 + self.n_accept as f64)
+    }
+
     fn accept_prob_ratio(&self, cur_sample: ArrayView1<f64>, prop_sample: ArrayView1<f64>) -> f64
     {
-        let lnp_diff = self.ln_prob_fn.lnp(prop_sample, Vec::new()) -
-                       self.ln_prob_fn.lnp(cur_sample, Vec::new());
-        let mut alpha: f64 = lnp_diff.max(1.0);
+        let lnp_diff = self.ln_prob_fn.lnp(prop_sample, &Vec::new()) -
+                       self.ln_prob_fn.lnp(cur_sample, &Vec::new());
+        let mut alpha: f64 = lnp_diff;
         alpha = alpha.max(0.0);
         alpha = alpha.min(1.0);
         alpha
@@ -320,5 +413,53 @@ mod space_samplers_unit_tests {
         for (_i, sample) in samples.axis_iter(Axis(0)).enumerate() {
             assert_approx_eq!(sample.sum(), 1.0);
         }
+    }
+
+    #[test]
+    fn test_demcmc_sampler_gauss() {
+        // Sample 1D Gaussian distribution using the mcmc sampler.
+        let tst_mu = 2.0;
+        let tst_std = 3.0;
+        // log likelihood up to a constant
+        let tst_gauss_pdf = move | x: ArrayView1<f64>, _b: &Vec<f64> | { (-0.5*((x[0]-tst_mu)/tst_std).powf(2.0)).exp()/tst_std } ;
+        let tst_gauss_ln_pdf = move | x: ArrayView1<f64>, _b: &Vec<f64> | { tst_gauss_pdf(x, _b).ln() } ;
+        // construct bounds prior
+        let bounds = Array2::from(vec![
+            [-20.0, 20.0]
+            ]);
+        let tst_ln_prior = LnPriorUniform::new(bounds.view());
+        // construct likelihood
+        let tst_ln_like = LnLikeCustom::new(tst_gauss_ln_pdf);
+        // construct likelihood*prior
+        let tst_ln_like_prior = LnLikeSum::new(tst_ln_like, tst_ln_prior);
+
+        // init the MCMC sampler chains
+        let x0 = Array1::from(vec![0.0]);
+        let mut tst_chains: Vec<McmcChain> = Vec::new();
+        let n_chains = 8;
+        for c in 0..n_chains {
+            tst_chains.push(McmcChain::new(1, x0.view(), c));
+        }
+        // init the MCMC sampler
+        let mut tst_mcmc_sampler = DeMcSampler::new(tst_ln_like_prior, tst_chains, 1, 0.8, 1.0e-10);
+
+        // draw samples
+        let n_samples: usize = 1000;
+        tst_mcmc_sampler.sample_mcmc(n_samples);
+
+        // Check the estimated mean and var against known]
+        let tst_samples = tst_mcmc_sampler.get_samples(500);
+        println!("Samples: {:?}", tst_samples);
+        println!("Accept ratio: {:?}", tst_mcmc_sampler.accept_ratio());
+
+    }
+
+    #[test]
+    fn test_demcmc_sampler_diriclet() {
+        // TODO: test ability to draw samples from
+        // constrained 3D diriclet dist.
+        // This is useful to greatly improve efficiency of drawing
+        // samples froma highly constrained diriclet sampling problem
+        // in high dimensions where rejection sampling alone is inefficient.
     }
 }
